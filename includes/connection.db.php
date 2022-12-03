461c1ff61f0133de4f43653bb7b5812b93296c82
Mihai Daniel <proiect.mihai.machete@gmail.com>
2022-12-03 22:28:51 +0200

    Update

--- includes/connection.db.php
+++ includes/connection.db.php
@@ -1,12 +1,24 @@
 <?php
 
-$serverName="localhost";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//////////////////////////////////////////////////
+/*$serverName="localhost";
 $dbUserName="root";
 $dbPassword="";
 $dbName="pr_orgz_ev";
 
-$conn=mysqli_connect($serverName, $dbUserName, $dbPassword, $dbName);
+$conn=mysqli_connect($serverName, $dbUserName, $dbPassword, $dbName);*/
 
-if(!$conn){
+//$conn = $conn2 || $conn1;
+
+ if(!$conn){
     die("Connection failed".mysqli_connect_error());
 }
