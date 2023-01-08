80baee0238f5069933d6c10d3ea2541bcc6e405f
Mihai Daniel <proiect.mihai.machete@gmail.com>
2023-01-08 14:10:08 +0200

    Verificare

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -2,11 +2,11 @@
 $secretkey = "6Ldh2twjAAAAAKE7TccF_TEvc6A-Z-CwUsRThlfn";
 
 if(isset($_POST["submit"])){
-  if(isset($_POST["g-recaptcha-response"]) && !empty($_POST["g-recaptcha-response"])){
-    $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretkey."&response=".$_POST["g-recaptcha-response"];
-    $response = file_get_contents($url);
-    $responseData= json_decode($response);
-    if($responseData->success){
+  // if(isset($_POST["g-recaptcha-response"]) && !empty($_POST["g-recaptcha-response"])){
+  //   $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretkey."&response=".$_POST["g-recaptcha-response"];
+  //   $response = file_get_contents($url);
+  //   $responseData= json_decode($response);
+  //   if($responseData->success){
 $username=$_POST["username"];
 $password=$_POST["pwd"];
 
@@ -20,13 +20,13 @@ if(emptyInputLogin($username, $password)){
    }
 
 loginUser($conn, $username, $password);   
-
-}}
-else{
-  header("location: ../login.php?error=recaptcha");
-  exit();
-   }
 }
+// }
+// else{
+//   header("location: ../login.php?error=recaptcha");
+//   exit();
+//    }
+// }
 else{
   header("location: ../login.php");
   exit();
